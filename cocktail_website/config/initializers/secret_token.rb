066415d72412048ae372d142affb778ebdca1852
Sam Drazin <sam.drazin@gmail.com>
2013-03-23 14:43:04 -0500

    created rails project

---
+++ cocktail_website/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CocktailWebsite::Application.config.secret_token = '04fd01aa2483095ae6d17b1e0c73215a1b3e603092517b2eb2500fc73370d0521dd8e74cbadcfeb09c0bfa6d0147f20e67a522203d6eb4ccc89558f1c2c865b0'
